78c3be3d4a28851023040d15b3c4e8d3dd2cc1b4
cornelia chirila <cornelia.calarasiu@yahoo.it>
2020-05-15 16:18:40 +0100

    Cocktails challenge day 2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,9 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 import 'bootstrap';
+import { initSelect2 } from '../components/init_select2';
+
+initSelect2();
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
